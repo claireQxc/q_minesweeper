9dc5002d441414935ebad6d36ebe67fd45aec99a
claire <qiuxiaocen@zhizukj.com>
2022-06-28 21:04:07 +0800

    feat: reconstructing

--- src/components/MineBlock.vue
+++ src/components/MineBlock.vue
@@ -4,7 +4,34 @@ import { isDev } from '~/composables'
 
 defineProps<{ block: BlockState }>()
 
+const emit = defineEmits<{ (e: 'lrclick', event: MouseEvent): void }>()
+
+function whichButton(event: MouseEvent) {
+  if (event.buttons === 3)
+    emit('lrclick', event)
+}
+
+const numberColors = [
+  'text-transparent',
+  'text-blue-500',
+  'text-green-500',
+  'text-yellow-500',
+  'text-orange-500',
+  'text-red-500',
+  'text-purple-500',
+  'text-pink-500',
+]
+
+function getBlockClass(block: BlockState) {
+  if (block.flagged)
+    return 'bg-gray-500/10'
+  if (!block.revealed)
+    return 'bg-gray-500/10 hover:bg-gray-500/20'
+
+  return block.mine ? 'bg-red-500/50' : numberColors[block.adjacentMines]
+}
 </script>
+
 <template>
   <button
     flex="~"
@@ -18,7 +45,7 @@ defineProps<{ block: BlockState }>()
     <template v-if="block.flagged">
       <div i-mdi:flag text-red />
     </template>
-    <template v-else-if="block.revealed || dev">
+    <template v-else-if="block.revealed || isDev">
       <div v-if="block.mine" i-mdi:mine />
       <div v-else font-600>
         {{ block.adjacentMines }}
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,148 +1,11 @@
 <script setup lang="ts">
+import { GamePlay } from '~/composables/logic'
 
-interface BlockState {
-  x: number
-  y: number
-  revealed: boolean
-  mine?: boolean
-  flagged?: boolean
-  adjacentMines: number
-}
+const play = new GamePlay(9, 9, 10)
 
-const WIDTH = 5
-const HEIGHT = 5
-let GAME_OVER = false
-const state = ref(
-  Array.from({ length: HEIGHT }, (_, y) =>
-    Array.from({ length: WIDTH },
-      (_, x): BlockState => ({
-        x, y, revealed: false, adjacentMines: 0,
-      }),
-    ),
-  ),
-)
+const now = $(useNow())
+const timerMS = $computed(() => Math.round())
 
-function generateMines(initial: BlockState) {
-  for (const row of state.value) {
-    for (const block of row) {
-      if (Math.abs(initial.x - block.x) < 1)
-        continue
-      if (Math.abs(initial.y - block.y) < 1)
-        continue
-      block.mine = Math.random() < 0.3
-    }
-  }
-}
-
-const directions = [
-  [1, 1],
-  [1, 0],
-  [1, -1],
-  [0, -1],
-  [-1, -1],
-  [-1, 0],
-  [-1, 1],
-  [0, 1],
-]
-function updateNumbers() {
-  state.value.forEach((raw, y) => {
-    raw.forEach((block, x) => {
-      if (block.mine)
-        return
-      getSiblings(block)
-        .forEach((b) => {
-          if (b.mine)
-            block.adjacentMines += 1
-        })
-    })
-  })
-}
-
-const numberColors = [
-  'text-transparent',
-  'text-blue-500',
-  'text-green-500',
-  'text-yellow-500',
-  'text-orange-500',
-  'text-red-500',
-  'text-purple-500',
-  'text-pink-500',
-]
-
-function getBlockClass(block: BlockState) {
-  if (block.flagged)
-    return 'bg-gray-500/10'
-  if (!block.revealed)
-    return 'bg-gray-500/10 hover:bg-gray/10'
-
-  return block.mine ? 'bg-red-500/50' : numberColors[block.adjacentMines]
-}
-
-function expandZero(block: BlockState) {
-  if (block.adjacentMines)
-    return
-  getSiblings(block).forEach((s) => {
-    if (!s.revealed) {
-      s.revealed = true
-      expandZero(s)
-    }
-  })
-}
-
-let mineGenerated = false
-const dev = false
-
-function onRightClick(block: BlockState) {
-  if (GAME_OVER || block.revealed)
-    return
-  block.flagged = !block.flagged
-  checkGameState()
-}
-
-function onClick(block: BlockState) {
-  if (GAME_OVER)
-    return
-  if (!mineGenerated) {
-    generateMines(block)
-    updateNumbers()
-    mineGenerated = true
-  }
-  block.revealed = true
-  if (block.mine) {
-    GAME_OVER = true
-    alert('BOOOOOM!')
-  }
-  expandZero(block)
-  checkGameState()
-}
-
-function getSiblings(block: BlockState) {
-  return directions.map(([dx, dy]) => {
-    const x2 = block.x + dx
-    const y2 = block.y + dy
-    if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT)
-      return undefined
-
-    return state.value[y2][x2]
-  })
-    .filter(Boolean) as BlockState []
-}
-
-function checkGameState() {
-  if (!mineGenerated || GAME_OVER)
-    return
-  const blocks = state.value.flat()
-  if (blocks.every(block => block.revealed || block.flagged)) {
-    if (blocks.some(block => block.flagged && !block.mine)) {
-      GAME_OVER = true
-      alert('You Cheat!')
-    }
-    else {
-      alert('You Win!')
-      GAME_OVER = true
-    }
-  }
-}
 </script>
 
 <template>
@@ -158,6 +21,7 @@ function checkGameState() {
         <mine-block
           v-for="block, x in row"
           :key="x"
+          :block="block"
         />
       </div>
     </div>
